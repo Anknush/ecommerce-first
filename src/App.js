4fe91ace3e814021f9f8e1741a350bfc4201567c
Anknush <42448076+Anknush@users.noreply.github.com>
2021-05-03 23:28:36 +0530

    Add Header/Navbar in the app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./homepage-components";
 import ShopData from "./pages/homepage/shop/shop-components";
+import Header from "./components/menu-component/header/header-component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopData} />
